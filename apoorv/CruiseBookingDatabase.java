60e562b7d07db5d1ed1fb3098055d5b32aafd444
Apoorv Srivastava <2000apoorv@gmail.com>
2020-10-15 10:11:02 +0530

    added database classes

---
+++ apoorv/CruiseBookingDatabase.java
@@ -0,0 +1,15 @@
+class CruiseBookingDatabase{
+    public void insertCruiseBooking(int bookingId , int shipId , int userId , int seats , int cost){
+
+    }
+    public void updateCruiseBooking(int bookingId , int shipId , int userId , int seats , int cost){
+
+    }
+    public void deleteCruiseBooking(int bookingId){
+
+    }
+
+    public CruiseBooking getUserCruiseBooking(int bookingId){
+
+    }
+}
